627bac6c1167260dc8efe571f8bea7bb4eae2e9a
Ahmadmasyruhan <121494906+Ahmadmasyruhan@users.noreply.github.com>
2023-06-06 20:56:15 +0700

    Update Employee.py
    merubah source code Employee

--- Employee.py
+++ Employee.py
@@ -10,10 +10,10 @@ class Employee:
       print("Total Employee %d" % Employee.empcount)
       
     def displayEmployee(self):
-      print("Name :",self.name, ",Salary: ",self.salary)
+      print("Name :",self.nama, ",Salary:",self.salary)
     
-    emp1 = Employee("Masyruhan" , 10000)
-    emp2 = Employee("M.irwan" ,7000)
-    emp1.displayEmployee()
-    emp2.displayEmployee()
-    print("total employee %d" % Employee.empCount)
+emp1 = Employee("Masyruhan" , 10000)
+emp2 = Employee("M.irwan" ,7000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print("total employee %d" % Employee.empCount)
